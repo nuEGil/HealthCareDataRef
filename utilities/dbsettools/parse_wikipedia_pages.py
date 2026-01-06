24d4fa2f91ad97ab373c63c342343b4bc741528c
Eddie <42759839+nuEGil@users.noreply.github.com>
2026-01-06 13:38:01 -0600

    implemented tfidf for keyword add

--- utilities/dbsettools/parse_wikipedia_pages.py
+++ utilities/dbsettools/parse_wikipedia_pages.py
@@ -1,47 +1,154 @@
 import os
 import re
-import time
 import glob
-import sqlite3
+import json
+import copy
+from collections import Counter
+from dataclasses import dataclass, field, asdict
+import math 
+
 from bs4 import BeautifulSoup
 '''
 get symptoms
 get description
-either add to icd10 db or make a new db 
-copy the old db so you dont have to run the old code if you mess this up. 
+
+Paths for this code.
+1. is if we have the search terms we used that were reading DB entries, then we can just read the wikipedia pages directly and add to a new database with symptoms
+2. there's a version for the demo where we read a set list of lung pathologies. we need different logic for that. 
+
+LogicA - icd10 to google search to wiki pages
+1. read google search jsons for the search terms. 
+2. parse the wikipedia pages 
+3. output results to sql database so it has - icd term + symptoms, + wiki page
+
+LogicB - set list of terms to google search to wiki pages
+1. get keywords especially symptoms from any wikipedia pages
+2. search icd10 database for a match given the key words
+
+
+but you still need to keep the same format... gonna need a data class. 
+
+
+ranking # term_frequency = sorted(counts.items(), key=lambda kv: (-kv[1], kv[0]))  # count desc, token asc)
+for tfidf batch it  - might have a wikipedia results dir that's too big. 
+that's interesting. the batching part 
 '''
+@dataclass
+class entry():
+    title: str = field(default_factory=str)
+    description: str = field(default_factory=str)
+    symptoms: str = field(default_factory=str)
+    keywords: str = field(default_factory=str)
+
+    # --- some parts to do TF IDF -- do not store these counters in db
+    term_frequencies: dict = field(default_factory=Counter) 
+    tfidf: dict = field(default_factory=dict)     
+    
+    def computeTermFrequency(self):
+        if len(self.term_frequencies) == 0:
+            pooled_text = self.symptoms + " " + self.description
+            clean_text = re.sub(r'[^0-9a-z]+', ' ', pooled_text.lower())
+            term_counts = Counter(clean_text.split())
+            denom = sum(term_counts.values())
+            for k,v in term_counts.items():
+                self.term_frequencies[k] = v/denom
+                self.tfidf[k] = 0.0
+
+@dataclass
+class TFIDF_computer():
+    entries:list 
+    doc_frequencies:dict  
+    
+    def compute_TFIDF(self): 
+        N = len(self.entries)
+        for aentry in self.entries:
+            for term, tf in aentry.term_frequencies.items():
+                # print('term :', term)
+                df = self.doc_frequencies[term]
+                idf = math.log((1 + N) / (1 + df)) + 1
+                aentry.tfidf[term] = math.log10(tf) + idf
+            # after you get the  tfidf sort them
+            top = sorted(aentry.tfidf.items(), key=lambda kv: (-kv[1], kv[0]))[0:10]
+            print(f"title:{aentry.title}, kw:{top}")
+            aentry.keywords=",".join([t[0] for t in top])
+            aentry.keywords+=f",{aentry.title}"
+            # aentry.sortTFIDF()
+                
+def getEntry(file_):
+    page_entrty = entry() # make an entry
+    
+    soup = BeautifulSoup(file_, 'html.parser')
+    title = soup.find_all(["title"])[0].text.strip()
+    page_entrty.title = title
+    
+    # this thing will find headings. so you just need 
+    heading_tags = ["title", "p", "li", "h1", "h2", "h3"]
+    desc_cond = False
+    description_text =""
+    
+    symptom_cond = False
+    symptoms_text = ""
+    
+    # edge detect - store all text between symptoms headings    
+    for tags in soup.find_all(heading_tags):
+        # -- Check for description first ---
+        if tags.name == "title":
+            desc_cond=True
+
+        if "h" in tags.name:
+            desc_cond=False
+
+        # -- check for symptoms --
+        if "h" in tags.name and "symptoms" in tags.text.strip().lower():
+            symptom_cond=True
+
+        if "h" in tags.name and not "symptoms" in tags.text.strip().lower():
+            symptom_cond=False
+            
+    
+        # Grab the description
+        if desc_cond and not symptom_cond:
+            description_text+=tags.text.strip()
+            description_text+="\n"
+
+        # grab the symptoms 
+        if symptom_cond and not desc_cond:
+            symptoms_text+=tags.text.strip()
+            symptoms_text+="\n"
+
+        page_entrty.description = description_text
+        page_entrty.symptoms = symptoms_text
+
+    return page_entrty
+
 
 if __name__ == '__main__':
+    lung_dat_path = os.path.join(os.environ["SEARCH_DB_PATH"], "lungdat/wikipedia_results/")
+    # lung_dat_path = os.path.join(os.environ["SEARCH_DB_PATH"], "base_crawl/wikipedia_results/")
     
-    pages = glob.glob(os.path.join("data/lungdat/wikipedia_results/", "*.html"))
+    pages = glob.glob(os.path.join(lung_dat_path, "*.html"))
     
-    for page in pages:
+    all_entries = []
+    doc_term_counter = Counter()
+    # need one loop to get the entries and get the 
+    for ii, page in enumerate(pages):
+        print("page number: ", ii)
         with open(page, "r") as f:
             file_ = f.read()
-        soup = BeautifulSoup(file_, 'html.parser')
-        title = soup.find_all(["title"])[0].text.strip()
+    
+        new_entry = getEntry(file_)
+        print(new_entry.title)
+        print(f"len desc :{len(new_entry.description)}  len symptoms: {len(new_entry.symptoms)}")
+        print(new_entry)
+        print('\n')
 
-        print('--------')
-        print("title :",title)
-        print('--------')
-        # this thing will find headings. so you just need 
-        heading_tags = ["p", "li", "h1", "h2", "h3"]
-        cond = False
-        symptoms_text =""
+        new_entry.computeTermFrequency() 
+        for term in new_entry.term_frequencies:
+            doc_term_counter[term]+=1
+        all_entries.append(new_entry)
 
-        # edge detect - store all text between symptoms headings    
-        for tags in soup.find_all(heading_tags):
-            if "h" in tags.name and "symptoms" in tags.text.strip().lower():
-                cond=True
-
-            if "h" in tags.name and not "symptoms" in tags.text.strip().lower():
-                cond=False
-
-            if cond:
-                symptoms_text+=tags.text.strip()
-                symptoms_text+="\n"
-        
-        print(symptoms_text)
-
-
-        
+    # print(doc_term_counter)
+    
+    coco = TFIDF_computer(entries=all_entries, doc_frequencies=doc_term_counter)
+    coco.compute_TFIDF()
+    print(coco.entries[0].title, ": ",coco.entries[0].keywords)
