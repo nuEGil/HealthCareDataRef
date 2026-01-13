6ac3a3bc17eb023eb66240459e14eb71c3c055a9
Eddie <42759839+nuEGil@users.noreply.github.com>
2026-01-12 20:26:37 -0600

    update. moved torch and tf to separate folders

--- utilities/dl_tools/chest_xray8/tensorflow/fine_tune_resnet.py
+++ utilities/dl_tools/chest_xray8/tensorflow/fine_tune_resnet.py
@@ -6,13 +6,15 @@ import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
 def df_to_dataset(df, image_size=64, batch_size=32, shuffle=True):
-    paths, labels = df["patch_path"].values, df["label_idx"].values
+    paths, labels = df["filename"].values, df["label"].values
     ds = tf.data.Dataset.from_tensor_slices((paths, labels))
     def load_img(path, label):
         img = tf.io.read_file(path)
         img = tf.image.decode_png(img, channels=3)
         img = tf.image.resize(img, [image_size, image_size])
+        lab = tf.one_hot(label, depth=2)
         return preprocess_input(img), label
+    
     ds = ds.map(load_img, num_parallel_calls=tf.data.AUTOTUNE)
     if shuffle: ds = ds.shuffle(len(df))
     return ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
@@ -37,7 +39,7 @@ def train_model():
     out = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
     model = tf.keras.models.Model(inputs=base.input, outputs=out)
     model.compile(optimizer="adam",
-                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+                   loss='categorical_crossentropy', 
                    metrics=["accuracy"])
     
     train_ds = df_to_dataset(train_df, image_size=128)
