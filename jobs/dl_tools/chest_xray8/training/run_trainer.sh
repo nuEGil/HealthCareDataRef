83576139b9f21468340acde53d049b1e909df3ad
Eddie <42759839+nuEGil@users.noreply.github.com>
2026-01-17 20:04:06 -0600

    update

--- jobs/dl_tools/chest_xray8/training/run_trainer.sh
+++ jobs/dl_tools/chest_xray8/training/run_trainer.sh
@@ -4,7 +4,7 @@ python jobs/dl_tools/chest_xray8/training/task.py \
 --data_set_dir NoF_Eff_Inf_Mas \
 --n_classes 4 \
 --learning_rate 0.001 \
---batch_size 32 \
+--batch_size 16 \
 --epochs 100
 
 # python jobs/dl_tools/chest_xray8/training/task.py \
